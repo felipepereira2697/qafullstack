a6d1cfed4880ddee2de67ca658b2fdee141112f4
felipepereira2697 <felipepereira2697@gmail.com>
2020-04-28 19:41:45 -0300

    Hashes on Ruby

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+#Data collection acessed as key -> value
+soccerPlayer = Hash[nome: 'Ronaldo', team: 'Corinthians']
+puts soccerPlayer[:team]
+
+soccerPlayer[:firstClub] = 'Cruzeiro'
+
+
+puts soccerPlayer

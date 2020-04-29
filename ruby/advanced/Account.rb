827e2d426abdde9300fc5818bd9d579bb8971832
felipepereira2697 <felipepereira2697@gmail.com>
2020-04-29 18:50:33 -0300

    Ruby constructor

--- ruby/advanced/Account.rb
+++ ruby/advanced/Account.rb
@@ -1,18 +1,23 @@
 class Account
 
-    attr_accessor :amount
+    attr_accessor :amount, :name
+    #Constructor
+    def initialize(name)
+        self.amount = 0.0
+        self.name = name;
+    end
 
     def deposit(value)
         #we can invoke methods or attributes of my class using self
         self.amount += value
-        puts "Now you have #{self.amount} "
+        puts "Hi #{self.name} now you have #{self.amount} "
     end
 
 end
 
-acc = Account.new
-acc.amount = 0.0
+acc = Account.new('Pereira')
 acc.deposit(1200.0)
 acc.deposit(100.0)
 
+puts acc.name
 puts acc.amount

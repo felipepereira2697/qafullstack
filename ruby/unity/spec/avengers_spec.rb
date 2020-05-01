a1364380d6b56aac9cd974e3a75c130b5cd8c50a
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-01 15:23:32 -0300

    Adding more test scenarios

--- ruby/unity/spec/avengers_spec.rb
+++ ruby/unity/spec/avengers_spec.rb
@@ -20,6 +20,44 @@ describe AvengersHeadQuarter do
         hq.put(avenger)
         #Expect avengertList to be equals to [avenger]
         expect(hq.avengerList).to eql [avenger]
+        expect(hq.avengerList).to include avenger
     end
+
+    it 'Should add an Avengers List' do
+
+        hq = AvengersHeadQuarter.new
+        hq.put('Thor')
+        hq.put('Spiderman')
+        hq.put('Ironman')
+        hq.put('Cap America')
+        #Check if on avengerList ironman is included
+        expect(hq.avengerList).to include 'Ironman'
+        expect(hq.avengerList).to include 'Cap America'
+        res = hq.avengerList.size > 0
+        expect(hq.avengerList.size).to be > 1
+        expect(res).to be true
+    end
+
+    it 'Cap America should be the first one' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Cap America')
+        hq.put('Thor')
+
+        expect(hq.avengerList).to start_with('Cap America') 
+    end
+
+    it 'Spiderman should be the last one' do
+        hq = AvengersHeadQuarter.new
+        hq.put('Cap America')
+        hq.put('Thor')
+        hq.put('Ironman')
+        hq.put('Dr Strange')
+        hq.put('Spiderman')
+
+        expect(hq.avengerList).to end_with('Spiderman')
+            
+    end
+
+
 
 end

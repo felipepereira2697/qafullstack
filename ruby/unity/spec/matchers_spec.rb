ab57e93e82651de78ad00157fed6c42e1ca3a640
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-01 17:51:06 -0300

    Arrays matchers

--- ruby/unity/spec/matchers_spec.rb
+++ ruby/unity/spec/matchers_spec.rb
@@ -8,4 +8,15 @@ describe 'Matchers' do
         expect(n1).to be(n2)
         expect(n1).to eq(n2)
     end
+
+    it 'Array matchers' do
+        array = [1, 2, 3, 4, 5, 14]
+        #TRUE FOR BOTH
+        expect(array).to match_array([1, 2, 3, 4, 5, 14])
+        expect(array).to match_array([14, 5, 4, 3, 2, 1])
+        expect(array).to include(14)
+        expect(array).to_not match_array([1, 2, 8, 7, 5, 14])
+    end
+
+
 end

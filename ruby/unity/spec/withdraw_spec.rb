0d8ea4bca8389375ba5fbd1b7aa3f8af4c1f984a
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-02 00:45:41 -0300

    Testing the remaining scnearios for RegularAccount

--- ruby/unity/spec/withdraw_spec.rb
+++ ruby/unity/spec/withdraw_spec.rb
@@ -5,7 +5,13 @@ class RegularAccount
         self.amount = value
     end
     def withdraw(value)
-        self.amount -= value
+        if value > self.amount
+            puts('Ooops insufficient value :( ')
+        elsif value > 200.0
+            puts('You can only take 200.0 per transaction')
+        else
+            self.amount -= value      
+        end
     end
 end
 #TEST SUITE
@@ -22,7 +28,25 @@ describe RegularAccount do
             expect(@acc.amount).to eql(800.00)
         end
        end
+       context 'When the value is insufficient' do
+        before(:all) do
+            @acc2 = RegularAccount.new(0.00)
+            @acc2.withdraw(100.0)
+        end
+        it 'notify user of insufficient value' do
+            expect(@acc2.amount).to eql(0.0)
+        end
+       end
 
+       context 'when the value is bigger than allowed per transaction' do
+        before(:all) do
+            @acc3 = RegularAccount.new(700.0)
+            @acc3.withdraw(500.0)
+        end
+        it 'notify user that can only take 200 per transaction' do
+            expect(@acc3.amount).to eql(700.0)
+        end
+       end
     end
 
 end

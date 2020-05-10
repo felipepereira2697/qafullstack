cd4513c535c67fdc3cfe4214863a992637fc8dbe
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-10 18:50:34 -0300

    Checkboxes, learning how to check and uncheck with Capybara

--- ruby/capy/spec/checboxes_spec.rb
+++ ruby/capy/spec/checboxes_spec.rb
@@ -1,6 +1,28 @@
-describe 'Checkboxes' do
+describe 'Checkboxes', :checkbox do
     
+    #Check and Uncheck methods works for names and Ids
+    before(:each) do
+
+        visit 'https://training-wheels-protocol.herokuapp.com/checkboxes'
+
+    end
     it 'Checking option' do
-        
+        check('thor')
+    end
+
+    it 'Uncheck option' do
+        uncheck('antman')
+
+    end
+
+    it 'find set true' do
+        find('input[value = cap]').set(true)
+    end
+
+    it 'uncheck with set false' do
+        find('input[value = guardians]').set(false)
+    end
+    after(:each) do
+        sleep 5
     end
 end

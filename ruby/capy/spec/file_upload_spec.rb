5b8976e3c92ed9c8609ad3c05002ccca5f1e0488
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-10 20:57:36 -0300

    Working and testing file uploads

---
+++ ruby/capy/spec/file_upload_spec.rb
@@ -0,0 +1,34 @@
+describe 'File upload', :upload do
+    
+    before(:each) do
+        
+        visit 'https://training-wheels-protocol.herokuapp.com/upload'
+        @filePath = Dir.pwd + '/spec/fixtures/file.txt'
+        @imgPath = Dir.pwd + '/spec/fixtures/star_8bit.jpg'
+
+    end
+
+
+    it 'Testing text file upload' do
+        puts @filePath
+        attach_file('file-upload', @filePath)
+
+        click_button 'Upload'
+
+        fileDiv = find('#uploaded-file')
+        expect(fileDiv.text).to eql 'file.txt'
+    end
+
+    it 'Testing image file upload' do
+        attach_file('file-upload', @imgPath)
+        click_button 'Upload'
+        sleep 5
+        img = find('#new-image')
+        #Since find('#new-image') returns basically the element, we can access its html attributes
+        expect(img[:src]).to include 'star_8bit.jpg'
+    end
+
+    after(:each) do
+        sleep 3
+    end
+end

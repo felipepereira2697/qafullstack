7da24f77e327a1f4c0107be40ae83536d61c55b9
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-11 22:10:19 -0300

    Awesome way to test drag and drop

---
+++ ruby/capy/spec/drag_drop_spec.rb
@@ -0,0 +1,15 @@
+describe 'Testing drag and drop', :dragdrop do
+    
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/drag_and_drop'
+    end
+    it 'Spiderman Stark team' do
+        #Acessing Stark div by class and element hierarchy
+        stark = find('.team-stark .column')
+
+        spider = find('img[alt$=Aranha]')
+        spider.drag_to stark
+
+        expect(stark).to have_css 'img[alt$=Aranha]'
+    end
+end

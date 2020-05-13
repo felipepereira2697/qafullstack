bc5ebef3cf4650f5681900d013817d7b5f5164ea
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-12 21:52:30 -0300

    Automated software test for selecting multiple actors

--- ruby/capy/spec/select2_spec.rb
+++ ruby/capy/spec/select2_spec.rb
@@ -24,9 +24,25 @@ describe 'Testing Select 2', :select2 do
     end
 
 
-    describe('multiple') do
+    describe('multiple', :mult) do
         before(:each) do
             visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/multi.html'
+        end
+
+        def selectActor(actor)
+            find('.select2-selection--multiple').click
+            sleep 2
+            find('.select2-search__field').set actor
+            find('.select2-results__option').click
+            sleep 5
+        end
+
+        it 'select multiple actors' do
+            actors = ['Chris Rock', 'Owen Wilson', 'Kevin James']
+            actors.each do |a|
+                selectActor(a)
+            end
+            sleep 5
         end
     end
 

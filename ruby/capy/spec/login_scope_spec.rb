6f15b3af1ada5df419d2ed759b231563d5a263c1
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-17 19:54:28 -0300

    Diff ambiguous elements using hierarchy

--- ruby/capy/spec/login_scope_spec.rb
+++ ruby/capy/spec/login_scope_spec.rb
@@ -1,34 +1,21 @@
-describe 'Login Random Forms', :random_login do
+describe 'Scope login', :scope_login do
 
     before(:each) do
-        visit 'https://training-wheels-protocol.herokuapp.com/login2'
+        visit 'https://training-wheels-protocol.herokuapp.com/access'
     end
-    it 'successful birth date login' do
-        
-
-        #Look for an element with name or id username and fill it with stark
-        find('#userId').set 'stark'
-        find('#passId').set 'jarvis!'
-
-        #search form and using Ruby case to work in parallel with Capybara
+    it 'Successful login' do
+        # Since we have two elements with input username and password we need to differ, one way to do it
+        #is using the parent, so in this case we`re gona use #login element as our parent
+        #so inside parent we can look for the element we want, this is a scope technique
+        #now the search is only on login structure and not on the whole page.
         login_form = find('#login')
-        #login_form.text amazing way these return all text related to that form
-        case login_form.text
-        when /Dia/
-            find('#day').set '29'
-        when /Mês/
-            find('#month').set '05'
-        when /Ano/
-            find('year').set '1970'
-        end
 
-        sleep 5
-        # click_button 'Login'
+        login_form.find('input[name=username').set 'stark'
+        login_form.find('input[name=password').set 'jarvis!'
 
-       
-
-        #Better way to do it --> this is from CAPYBARA not from RSpec
-        # expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
+        click_button 'Entrar'
+        sleep 3
+        expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end
 
     

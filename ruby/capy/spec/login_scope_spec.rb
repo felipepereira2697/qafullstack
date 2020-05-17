8beb46d5fd56490ffd71918b459d2abcee462457
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-17 19:58:13 -0300

    Refactoring in order to use capybara WITHIN method

--- ruby/capy/spec/login_scope_spec.rb
+++ ruby/capy/spec/login_scope_spec.rb
@@ -8,12 +8,20 @@ describe 'Scope login', :scope_login do
         #is using the parent, so in this case we`re gona use #login element as our parent
         #so inside parent we can look for the element we want, this is a scope technique
         #now the search is only on login structure and not on the whole page.
-        login_form = find('#login')
+        
 
-        login_form.find('input[name=username').set 'stark'
-        login_form.find('input[name=password').set 'jarvis!'
+        #One way to work with scope
+        #login_form = find('#login')
+        # login_form.find('input[name=username').set 'stark'
+        # login_form.find('input[name=password').set 'jarvis!'
 
-        click_button 'Entrar'
+        #Using Capybara within method
+        within('#login') do
+            login_form.find('input[name=username').set 'stark'
+            login_form.find('input[name=password').set 'jarvis!'
+            click_button 'Entrar'
+        end
+        
         sleep 3
         expect(find('#flash')).to have_content 'Olá, Tony Stark. Você acessou a área logada!'
     end

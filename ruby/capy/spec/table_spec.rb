577e6571f881022caf12159374599c21c156c9a9
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-19 21:39:08 -0300

    First table tests

--- ruby/capy/spec/table_spec.rb
+++ ruby/capy/spec/table_spec.rb
@@ -3,12 +3,10 @@ describe 'Testing tables', :table do
     before(:each) do
         visit 'https://training-wheels-protocol.herokuapp.com/tables'
     end
-
-    it 'Get Chris Evans inside actor list' do
-        expect(page).to have_content 'Chris Evans'
-    end
-
     it 'Chadwick Boseman Salary' do
+        # get all rows
+        find('table tbody tr')
+        
         expect(page).to have_content '700.000'
     end
 end

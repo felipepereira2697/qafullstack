f41a4f6e53e8397463160613db5df188aea7db74
felipepereira2697 <felipepereira2697@gmail.com>
2020-05-19 21:43:20 -0300

    Awesome, bringing the correct line and checking values with Ruby detect method

--- ruby/capy/spec/table_spec.rb
+++ ruby/capy/spec/table_spec.rb
@@ -4,9 +4,16 @@ describe 'Testing tables', :table do
         visit 'https://training-wheels-protocol.herokuapp.com/tables'
     end
     it 'Chadwick Boseman Salary' do
-        # get all rows
-        find('table tbody tr')
+        # get all rows but find should return ONLY ONE ELEMENT
+        #find('table tbody tr')
+        actors = all('table tbody tr')
+
+        #RUBY CODE
+        blackPanther = actors.detect{
+            |item| item.text.include?('Chadwick Boseman')
+        }
+        puts blackPanther.text
         
-        expect(page).to have_content '700.000'
+        expect(blackPanther.text).to include '700.000'
     end
 end
